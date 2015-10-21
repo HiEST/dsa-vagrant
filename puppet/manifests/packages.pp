a5433fc4d6fae69e9b60449639ce4fe9a1bcb370
David Carrera <dcarrera@ac.upc.edu>
2015-10-21 15:55:34 +0200

    added ompss

--- puppet/manifests/packages.pp
+++ puppet/manifests/packages.pp
@@ -2,13 +2,26 @@ exec { 'apt-get update':
   path => '/usr/bin'
 }
 
-#package { ['linux-image-generic-lts-trusty']:
-#  ensure => present,
-#  before => Exec['apt-get update']
-#}
+apt::source { 'ompss-repository':
+  comment  => 'BSC PM repository',
+  location => 'http://pm.bsc.es/ompss/packages/debian',
+  release  => 'wheezy',
+  repos    => 'main',
+  require => Exec['omps-pgp'],
+  before  => Exec['apt-get update']
+}
 
+exec { "omps-pgp":
+   cwd     => "/tmp",
+   command => "wget -O - http://pm.bsc.es/ompss/packages/debian/ompss.gpg.key | apt-key add -",
+   path    => "/usr/local/bin/:/usr/bin:/bin/",
+   user    => 'root',
+   timeout => 0
+}
 
-package { ['curl', 'unzip', 'vim', 'make', 'gcc', 'g++', 'automake', 'libtool', 'libibverbs-dev', 'librdmacm-dev', 'ibverbs-utils', "linux-headers-$kernelrelease", 'rdmacm-utils','libibcm1', 'tree', "linux-image-extra-$kernelrelease"]:
+#unless => "grep /etc/apt/sources.list.d/",
+
+package { ['curl', 'unzip', 'vim', 'make', 'gcc', 'g++', 'automake', 'libtool', 'libibverbs-dev', 'librdmacm-dev', 'ibverbs-utils', "linux-headers-$kernelrelease", 'rdmacm-utils','libibcm1', 'tree', "linux-image-extra-$kernelrelease", 'ompss']:
   ensure => present,
   require => Exec['apt-get update']
 }
